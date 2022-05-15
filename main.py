6375b61a9ff16f18a84280c4547d6fb354c4befc
Paul M Vega <Paul@macbook-pro-2.lan>
2022-05-15 16:51:07 -0500

    added additional code lines to main.py daytrip project

--- main.py
+++ main.py
@@ -12,25 +12,39 @@ thing_to_do_ORL = ["Disney", "Universal", "Hard Rock"]
 
 def trip_destination (place_to_travel):
     rand_destination = random.choice (place_to_travel) 
-    print (rand_destination)
+    print (f'Would you like to go to {rand_destination}?')
+    user_destination_choice = input("y or n? ")
+    if user_destination_choice != "y":
+        trip_destination (place_to_travel)
+    elif user_destination_choice == "y": 
+        print ("Awesome, lets go!")
+        
+      
+        
 
 def travel_mode (mode_to_travel):
     rand_travel_mode = random.choice (mode_to_travel)
+    print (f'Would you like to go by {mode_to_travel}?')
+    travel_mode_choice = input("y or n? ")
+    if travel_mode_choice != "y":
+        travel_mode (place_to_travel)
+    elif travel_mode_choice == "y": 
+        print ("Awesome, lets ride!")
     print (rand_travel_mode)
 
-# need to apply random selction based on destination
+# # need to apply random selction based on destination
 
-def dinner_plans (place_to_eat_LAS):
-    rand_eatery = random.choice (place_to_eat_LAS)
-    print (rand_eatery)
+# def dinner_plans (place_to_eat_LAS):
+#     rand_eatery = random.choice (place_to_eat_LAS)
+#     print (rand_eatery)
 
-# need to apply random selection based on destination   
+# # need to apply random selection based on destination   
 
-def entertainment_plans (entertainment_places):
-    rand_entertainment = random.choice (entertainment_places)
-    print (rand_entertainment)
+# def entertainment_plans (entertainment_places):
+#     rand_entertainment = random.choice (entertainment_places)
+#     print (rand_entertainment)
 
 trip_destination(place_to_travel)
 travel_mode (mode_to_travel)
-dinner_plans (place_to_eat_LAS)
-entertainment_plans (place_to_eat_LAS)
+# dinner_plans (place_to_eat_LAS)
+# entertainment_plans (thing_to_do_LAS)
