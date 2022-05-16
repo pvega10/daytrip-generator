2ef337f6e4d1d4aaffe43365148f170e9658ad9f
Paul M Vega <Paul@macbook-pro-2.lan>
2022-05-16 17:34:11 -0500

    completed the random generastor, added a debug file

--- main.py
+++ main.py
@@ -1,56 +1,72 @@
 import random
 
 
+#only use 4 lists, dont worry about having to have a specific place for each location
+place_to_travel = ["San Antonio", "Las Vegas", "Orlando", "Denver"]
+mode_to_travel = ["car", "train", "airline"]
+place_to_eat= ["a brewpub", "The Hard Rock", "a fast food eatery", "a fine dining restaurant"]
+thing_to_do = ["comedy club", "museum", "theatre", "movies"]
 
-place_to_travel = ["San Antonio", "Las Vegas", "Orlando"]
-mode_to_travel = ["Rental Car", "Train", "Airline"]
-place_to_eat_LAS = ["Luxor", "MGM", "Bellagio"]
-place_to_eat_SAT = ["Mi Tierra", "Mon Chu Chu", "ChartHouse"]
-place_to_eat_ORL = ["Bubba Gump", "House of Blues", "Millers Ale House"]
-thing_to_do_LAS = ["Cirque Show", "Pawn Stars", "High Roller"]
-thing_to_do_SAT = ["Alamo", "River Walk", "The Pearl"]
-thing_to_do_ORL = ["Disney", "Universal", "Hard Rock"]
 
 def trip_destination (place_to_travel):
     rand_destination = random.choice (place_to_travel) 
-    print (f'Would you like to go to {rand_destination}? y or n? ')
-    user_destination = input()
-    if user_destination != "y":
+    print (f'Would you like to go to {rand_destination}? y or n?')
+    user_destination_choice = input()
+    if user_destination_choice != "y":
         trip_destination (place_to_travel)
-    elif user_destination == "y": 
+    elif user_destination_choice == "y": 
         print ("Awesome, lets go!")
+        return rand_destination
         
       
         
-# def travel_mode (mode_to_travel):
-#     rand_travel_mode = random.choice (mode_to_travel)
-#     print (f'Would you like to go by {rand_travel_mode}?')
-#     travel_mode_choice = input("y or n? ")
-#     if travel_mode_choice != "y":
-#         travel_mode (mode_to_travel)
-#     elif travel_mode_choice == "y": 
-#         print ("Great! Sounds fun!")
+def travel_mode (mode_to_travel):
+    rand_travel_mode = random.choice (mode_to_travel)
+    print (f'Would you like to go by {rand_travel_mode}? y or n?')
+    user_travel_mode_choice = input()
+    if user_travel_mode_choice != "y":
+        travel_mode (mode_to_travel)
+    elif user_travel_mode_choice == "y": 
+        print ("Great! Sounds fun!")
+        return rand_travel_mode
+
     
 
-# # need to apply random selction based on destination
 
-# def dinner_plans_vegas (place_to_eat_LAS):
-#     if travel_mode == "Las Vegas":
-#         rand_eatery = random.choice (place_to_eat_LAS)
-#         print (f'Would you like to eat at {rand_eatery}')
-        # LAS_eatery_choice = input ("y or n? ")
-        #     if LAS_eatery_choice != "y":
-        #         dinner_plans (place_to_eat_LAS)
+
+def dinner_plans (place_to_eat):
+    rand_food = random.choice (place_to_eat)
+    print (f'Would you like to eat at {rand_food}? y or n?')
+    user_food_choice = input()
+    if user_food_choice != "y":
+        dinner_plans (place_to_eat)
+    elif user_food_choice == "y": 
+        print ("Great! Sounds fun!")
+        return rand_food
             
-    
+      
 
-# # need to apply random selection based on destination   
+def entertainment_plans (thing_to_do):
+    rand_entertainment = random.choice (thing_to_do)
+    print (f'Would you like to go to the {rand_entertainment}? y or n?')
+    user_entertainment_choice = input()
+    if user_entertainment_choice != "y":
+        entertainment_plans (thing_to_do)
+    elif user_entertainment_choice == "y":
+        print ("Fun Stuff!")
+        return rand_entertainment
 
-# def entertainment_plans (entertainment_places):
-#     rand_entertainment = random.choice (entertainment_places)
-#     print (rand_entertainment)
 
-trip_destination(place_to_travel)
-# travel_mode (mode_to_travel)
-# dinner_plans (place_to_eat_LAS)
-# entertainment_plans (thing_to_do_LAS)
+# add a variable infront of each function call, you will also need to make sure to add a return to each function 
+
+confirmed_destination = trip_destination (place_to_travel)
+confirmed_travel_mode = travel_mode (mode_to_travel)
+confirmed_dinner_plans = dinner_plans (place_to_eat)
+confirmed_entertainment_plans = entertainment_plans (thing_to_do)
+
+
+# print (confirmed_destination)
+# print (confirmed_travel_mode)
+# print (confirmed_dinner_plans)
+# print (confirmed_entertainment_plans)
+print (f'Congratulations, youll be going to {confirmed_destination} by {confirmed_travel_mode}, where you will eat at {confirmed_dinner_plans} and then go to the {confirmed_entertainment_plans}')
